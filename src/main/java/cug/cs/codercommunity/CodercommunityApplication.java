a81054af229914a567a6c2f34f3adf496da437ab
isHaitaoLiu <lht1970847795@gmail.com>
2021-06-01 17:09:04 +0800

    init repository

---
+++ src/main/java/cug/cs/codercommunity/CodercommunityApplication.java
@@ -0,0 +1,13 @@
+package cug.cs.codercommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodercommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodercommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cug/cs/codercommunity/CodercommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cug.cs.codercommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodercommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
